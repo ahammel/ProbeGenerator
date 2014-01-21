80834f60b99aec5655a4ab1edf4a0ee2037bdd17
Alex Hammel <ahammel@bcgsc.ca>
2014-01-21 11:06:34 -0800

    Added UI to generate probes from cooridinates
        Statements are now read from files, rather than passed in at the
        command line.

--- probe_generator/__main__.py
+++ probe_generator/__main__.py
@@ -1,16 +1,26 @@
 """Automatically generate probe sequences.
 
 Usage:
+    probe-generator --statement STMT  --genome GENOME --annotation FILE...
     probe-generator --coordinate COORD  --genome GENOME
 
 Options:
-    -c COORD --coordinate=COORD     a coordinate statement
+    -c COORD --coordinate=COORD     a file contatinng coordinate statements
                                     e.g. "1:100-10/2:200+20"
+    -s STMT --statement=STMT        a file containg fusion statements
+                                    e.g. "FOO#exon[1]-10/BAR#exon[2]+20"
     -g GENOME --genome=GENOME       the Ensembl reference genome
                                     (FASTA format)
+    -a FILE --annotation=FILE       a genome annotation file in UCSC format
 
 """
-from probe_generator import reference, coordinate_statement
+import itertools
+
+from probe_generator import (reference,
+                             coordinate_statement,
+                             annotation,
+                             probe_statement,
+                             sequence)
 
 from docopt import docopt
 
@@ -26,11 +36,22 @@ def reverse_complement(string):
     return ''.join(reversed(string.translate(_COMPLEMENT)))
 
 
-def print_fasta(head, sequence):
+def print_fasta(head, bases):
     """Print a single string in FASTA format.
 
     """
-    print(">{}\n{}\n".format(head, sequence))
+    print(">{}\n{}".format(head, bases))
+
+
+def combine_annotations(annotation_files):
+    """Given a list of annotation files, return a single annotation.
+
+    """
+    rows = []
+    for annotation_file in annotation_files:
+        with open(annotation_file) as handle:
+            rows.extend(annotation.parse_ucsc_file(handle))
+    return rows
 
 
 def bases_from_coordinate(coordinate, ref_genome):
@@ -52,21 +73,53 @@ def bases_from_coordinate(coordinate, ref_genome):
     return first_bases + second_bases
 
 
-def print_probe_from_coordinate(statement, genome_file):
-    """Print a probe in FASTA format given a coordinate statement and a
-    reference genome file.
+def print_probes_from_coordinate(statements_file, genome_file):
+    """Print a probe in FASTA format given a reference genome file and a file
+    containing coordinate statements.
 
     """
-    with open(genome_file) as handle:
-        ref_genome = reference.reference_genome(handle)
-        coordinate = coordinate_statement.parse(statement)
-        bases = bases_from_coordinate(coordinate, ref_genome)
-        print_fasta(statement, bases)
+    with open(genome_file) as genome, open(statements_file) as statements:
+        ref_genome = reference.reference_genome(genome)
+        for statement in statements:
+            statement = statement.strip()
+            coordinate = coordinate_statement.parse(statement)
+            bases = bases_from_coordinate(coordinate, ref_genome)
+            print_fasta(statement, bases)
+
+
+def print_probes_from_statements(statements_file, genome_file, annotation_files):
+    """Print probes in FASTA format give a rerence genome file, a file
+    containing probe statements, and UCSC genome annotations.
+
+    """
+    combined_annotation = combine_annotations(annotation_files)
+    with open(statements_file) as statements, open(genome_file) as genome:
+        ref_genome = reference.reference_genome(genome)
+        for statement in statements:
+            statement = statement.strip()
+            specification = probe_statement.parse(statement)
+            expanded_specifications = probe_statement.expand(specification)
+            for expanded_specification in expanded_specifications:
+                left_rows = annotation.lookup_gene(
+                        specification['gene1'], combined_annotation)
+                right_rows = annotation.lookup_gene(
+                        specification['gene2'], combined_annotation)
+                for left, right in itertools.product(left_rows, right_rows):
+                    coordinate = sequence.sequence_range(
+                            specification, left, right)
+                    bases = bases_from_coordinate(coordinate, ref_genome)
+                    header = "{} {} {}".format(
+                            statement, right['name'], left['name'])
+                    print_fasta(header, bases)
 
 
 def main():
     args = docopt(__doc__, version='ProbeGenerator {}'.format(VERSION))
-    print_probe_from_coordinate(args['--coordinate'], args['--genome'])
+    if args['--coordinate'] is not None:
+        print_probes_from_coordinate(args['--coordinate'], args['--genome'])
+    else:
+        print_probes_from_statements(
+                args['--statement'], args['--genome'], args['--annotation'])
 
 
 if __name__ == '__main__':
