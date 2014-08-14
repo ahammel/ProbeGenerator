61be3c0dfd84d1ffc5f3b7b324b2da6ced15907f
Alex Hammel <ahammel@bcgsc.ca>
2014-08-14 13:31:54 -0700

    Fixes from Assembly-75

--- probe_generator/variant.py
+++ probe_generator/variant.py
@@ -5,17 +5,33 @@ substitutions, etc.)
 from probe_generator.sequence import SequenceRange
 
 class AbstractVariant(object):
-    def __init__(self, *, transcript, index, reference, mutation, bases):
+    """Super-class for Variant objects.
+
+    The *Variant collection of classes represent arbitrary-length sequence
+    substitutions. Common uses include one-for-one substitutions (SNPs),
+    three-for-three (codon substitutions) and n-for-n (indels).
+
+    The super-class enforces the __init__ method and provides __len__ (which
+    just returns the length provided at init time).
+
+    `length` is the sum of the length of the variant and the surrounding buffer
+    sequence.
+
+    """
+    def __init__(self, *, transcript, index, reference, mutation, length):
         self.transcript = transcript
         self.index = index
         self.reference = reference
         self.mutation = mutation
-        self.bases = bases
+        self._length = length
 
         self.gene = self.transcript.gene_id
         self.transcript_name = self.transcript.name
         self.chromosome = self.transcript.chromosome
         self.coordinate = self.index.start + 1
+
+    def __len__(self):
+        return self._length
 
 
 class TranscriptVariant(AbstractVariant):
@@ -27,12 +43,12 @@ class TranscriptVariant(AbstractVariant):
 
     def sequence_ranges(self):
         chromosome, start, _end, _, _ = self.index
-        reference_bases = len(self.reference)
+        reference_length = len(self.reference)
 
-        reference_bases = len(self.reference)
-        mutation_bases = len(self.mutation)
+        reference_length = len(self.reference)
+        mutation_length = len(self.mutation)
 
-        total_buffer = self.bases - mutation_bases
+        total_buffer = self.length - mutation_length
         left_buffer = total_buffer // 2
         right_buffer = total_buffer - left_buffer
 
@@ -45,11 +61,12 @@ class TranscriptVariant(AbstractVariant):
             self.transcript.transcript_range(base-left_buffer, base) +
             [SequenceRange(chromosome,
                            start,
-                           start+reference_bases,
+                           start+reference_length,
                            mutation=self.mutation,
                            reverse_complement=not self.transcript.plus_strand)] +
-            self.transcript.transcript_range(base+reference_bases,
-                                 base+reference_bases+right_buffer))
+            self.transcript.transcript_range(base+reference_length,
+                                             base+reference_length+right_buffer)
+            )
 
         if self.transcript.plus_strand:
             return sequence
@@ -67,10 +84,10 @@ class GenomeVariant(AbstractVariant):
     def sequence_ranges(self):
         chromosome, start, _end, _, _ = self.index
 
-        reference_bases = len(self.reference)
-        mutation_bases = len(self.mutation)
+        reference_length = len(self.reference)
+        mutation_length = len(self.mutation)
 
-        total_buffer = self.bases - mutation_bases
+        total_buffer = self.length - mutation_length
         left_buffer = total_buffer // 2
         right_buffer = total_buffer - left_buffer
 
@@ -80,9 +97,9 @@ class GenomeVariant(AbstractVariant):
                           start),
             SequenceRange(chromosome,
                           start,
-                          start+reference_bases,
+                          start+reference_length,
                           mutation=self.mutation,
                           reverse_complement=not self.transcript.plus_strand),
             SequenceRange(chromosome,
-                          start+reference_bases,
-                          start+reference_bases+right_buffer))
+                          start+reference_length,
+                          start+reference_length+right_buffer))
